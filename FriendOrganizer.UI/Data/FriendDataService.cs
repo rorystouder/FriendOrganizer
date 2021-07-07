f0367f6de3f0f93099c8f40257cedb0ad947a1c4
rstouder <rstouder@intrasect.com>
2021-07-07 15:07:39 -0400

    Create the MainViewModel Implement INotifyPropertyChanged Event Set the Data Context

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from a real database
+            yield return new Friend { FirstName = "Rory", LastName = "Stouder" };
+            yield return new Friend { FirstName = "Alicia", LastName = "Fennig" };
+            yield return new Friend { FirstName = "Dylan", LastName = "Stouder" };
+            yield return new Friend { FirstName = "Gianna", LastName = "Stouder" };
+        }
+    }
+}
