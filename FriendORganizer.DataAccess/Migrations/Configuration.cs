d53db2eb1da5fc3eb29969f6280930b63703eb5b
rstouder <rstouder@intrasect.com>
2021-07-08 11:33:26 -0400

    Add a Code First Migration

---
+++ FriendORganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,28 @@
+﻿namespace FriendORganizer.DataAccess.Migrations
+{
+    using FriendOrganizer.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FriendORganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FriendORganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Friend { FirstName = "Rory", LastName = "Stouder" },
+                new Friend { FirstName = "Randy", LastName = "Stouder" },
+                new Friend { FirstName = "Jane", LastName = "Stouder" },
+                new Friend { FirstName = "Nick", LastName = "Stouder" },
+                new Friend { FirstName = "Scott", LastName = "Stouder" }
+                );
+        }
+    }
+}
