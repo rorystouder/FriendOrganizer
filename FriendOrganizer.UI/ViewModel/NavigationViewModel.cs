11939345fdb387023451aae9170793c66d422097
rstouder <rstouder@intrasect.com>
2021-07-09 09:52:56 -0400

    added Communication between ViewModels added prisim references

--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookupDataService _friendLookupService;
+        private IEventAggregator _eventAggregator;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupService)
+        public NavigationViewModel(IFriendLookupDataService friendLookupService,
+            IEventAggregator eventAggregator)
         {
             _friendLookupService = friendLookupService;
+            _eventAggregator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
 
@@ -25,5 +30,23 @@ namespace FriendOrganizer.UI.ViewModel
             }
         }
         public ObservableCollection<LookupItem> Friends { get; }
+
+        private LookupItem _selectedFriend;
+
+        public LookupItem SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set 
+            { 
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(_selectedFriend.Id);
+                }
+            }
+        }
+
     }
 }
