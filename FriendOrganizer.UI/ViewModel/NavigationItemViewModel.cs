65949c1bed5f0f785f54c328afc7462c06d16c38
rstouder <rstouder@intrasect.com>
2021-07-13 11:19:28 -0400

    Remove selectionability in the navigation View Model

--- FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
@@ -1,13 +1,23 @@
-﻿namespace FriendOrganizer.UI.ViewModel
+﻿using FriendOrganizer.UI.Event;
+using Prism.Commands;
+using Prism.Events;
+using System;
+using System.Windows.Input;
+
+namespace FriendOrganizer.UI.ViewModel
 {
     public class NavigationItemViewModel : ViewModelBase
     {
+        private IEventAggregator _eventAggregator;
         private string _displayMember;
 
-        public NavigationItemViewModel(int id, string displayMember)
+        public NavigationItemViewModel(int id, string displayMember,
+            IEventAggregator eventAggregator)
         {
+            _eventAggregator = eventAggregator;
             Id = id;
             DisplayMember = displayMember;
+            OpenFriendDetailViewCommand = new DelegateCommand(OnOpenFriendDetailView);
         }
 
         public int Id { get; }
@@ -20,6 +30,14 @@
                 _displayMember = value;
                 OnPropertyChanged();
             }
+        }
+
+        public ICommand OpenFriendDetailViewCommand { get; }
+
+        private void OnOpenFriendDetailView()
+        {
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(Id);
         }
     }
 }
